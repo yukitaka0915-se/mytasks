3836b1434e0726f06820b02c620f7e76e67cd94f
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-01-13 19:05:03 +0900

    create start signup

--- db/migrate/20200113094024_devise_create_users.rb
+++ db/migrate/20200113094024_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -32,10 +33,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      ## Authority
+      t.boolean :authority,               null: false, default: false
 
       t.timestamps null: false
     end
 
+    add_index :users, :name,                 unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
