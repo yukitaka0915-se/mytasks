7ceacf0a473273b39ea803a37d77db335a84fe78
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-01-15 13:50:25 +0900

    cerate contoroller and model
    SSIA

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,3 @@
 class GroupsController < ApplicationController
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "tasks#index"
   resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
